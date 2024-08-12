07863b58080371d21dad212acff097360ddcb0f6
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-08-12 14:49:01 +0200

    added loading of shopping cards

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-import { getResponseFromGetRequest } from './modules/network';
-import { setLocalStorageItem, getLocalStorageFromSingleKey} from './modules/storage';
+import { getResponseFromGetRequest } from './modules/network.js';
+import { setLocalStorageItem, getLocalStorageFromSingleKey} from './modules/storage.js';
+import { createItemCard } from './modules/ui.js';
 
+const cardContainer = document.getElementById("card-container");
 
+const storeData = await getResponseFromGetRequest('https://fakestoreapi.com/products');
+
+console.log(storeData);
+
+storeData.forEach(element => {
+    const itemId = self.crypto.randomUUID();
+    const newItemCard = createItemCard(element, itemId);
+
+    cardContainer.appendChild(newItemCard);
+});
+
+shoppingCartArray = [];
 
 
 
--- modules/ui.js
+++ modules/ui.js
@@ -1,25 +1,27 @@
 // DOM manipulation functions
 
-export function createItemCard(object, itemId) {
+export function createItemCard(dataObject, objectId) {
   const cardContainer = document.createElement("div");
   cardContainer.classList.add(
+    
     "bg-white",
     "shadow-lg",
     "rounded-lg",
-    "overflow-hidden"
+    "overflow-hidden",
+    "content-end"
   );
   const cardImg = document.createElement("img");
-  cardImg.classList.add("w-full", "h-56", "object-cover", "object-center");
-  cardImg.src = object.image;
-  cardImg.alt = `Image of ${object.title}`;
+  cardImg.classList.add("w-full", "h-32", "object-contain", "object-center");
+  cardImg.src = dataObject.image;
+  cardImg.alt = `Image of ${dataObject.title}`;
   const cardDetails = document.createElement("div");
   cardDetails.classList.add("p-4");
   const cardHeader = document.createElement("h3");
-  cardHeader.classList.add("text-gray-700", "font-bold", "text-xl");
-  cardHeader.textContent = object.title;
+  cardHeader.classList.add("text-gray-700", "font-bold", "text-base");
+  cardHeader.textContent = dataObject.title;
   const cardPrize = document.createElement("p");
   cardPrize.classList.add("mt-2", "text-gray-600");
-  cardPrize.textContent = object.price;
+  cardPrize.textContent = dataObject.price;
   const cardBtn = document.createElement("a");
   cardBtn.classList.add(
     "block",
@@ -32,9 +34,28 @@ export function createItemCard(object, itemId) {
     "rounded",
     "hover:bg-blue-600"
   );
-  cardBtn.href = itemId;
+  cardBtn.href = objectId;
   cardBtn.textContent = "Add to Shopping Cart"
+
+  cardDetails.appendChild(cardHeader);
+  cardDetails.appendChild(cardPrize);
+  cardDetails.appendChild(cardBtn);
+
+  cardContainer.appendChild(cardImg);
+  cardContainer.appendChild(cardDetails);
+
+  return cardContainer;
 }
+
+
+
+
+
+
+
+
+
+
 
 {
   /* <div class="bg-white shadow-lg rounded-lg overflow-hidden">
